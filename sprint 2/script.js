ad728b0df3e175214d051808d4c3d095ff39787a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-12 21:31:41 -0300

    Adição da declaração dos elementos

---
+++ sprint 2/script.js
@@ -0,0 +1,12 @@
+// Declaração dos elementos
+
+const infoBotao = document.querySelector('.info_botao')
+const info = document.querySelector('.info')
+
+const acessibilidade = document.querySelector('.acessibilidade')
+const roda = document.querySelector('.roda')
+
+const flash = document.querySelector('.flash')
+const timer = document.querySelector('.timer')
+const foto = document.querySelector('.foto')
+const galeria = document.querySelector('.galeria')
